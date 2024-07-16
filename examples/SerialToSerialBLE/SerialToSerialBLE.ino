0a334cc008cf4a7e3f5afeec00ab49422cd3c82d
Leonid Meleshin <hello@leon0399.ru>
2024-07-16 15:07:10 +0400

    📝 Add ETL usage example

--- examples/SerialToSerialBLE/SerialToSerialBLE.ino
+++ examples/SerialToSerialBLE/SerialToSerialBLE.ino
@@ -1,8 +1,18 @@
 #include <BLESerial.h>
 
+// FOR ETL: Uncomment the following lines
+// #include <Embedded_Template_Library.h>
+// #include <etl/queue.h>
+// #include <etl/circular_buffer.h>
+
 String device_name = "ESP32-BLE-Slave";
 
 BLESerial<> SerialBLE;
+
+// FOR ETL: Uncomment one of the following lines
+// BLESerial<etl::queue<uint8_t, 255, etl::memory_model::MEMORY_MODEL_SMALL>> SerialBLE;
+// OR
+// BLESerial<etl::circular_buffer<uint8_t, 255>> SerialBLE;
 
 void setup() {
     Serial.begin(9600);
@@ -12,6 +22,7 @@ void setup() {
 void loop() {
     if (Serial.available()) {
         SerialBLE.write(Serial.read());
+        SerialBLE.flush();
     }
     if (SerialBLE.available()) {
         Serial.write(SerialBLE.read());
