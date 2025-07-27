f38557d7c8e58706b6190d7a6aacfc695af9cab5
Leonid Meleshin <hello@leon0399.ru>
2025-07-27 05:35:41 +0200

    ✏️ Update comments

--- examples/SerialToSerialBLE_Secure/SerialToSerialBLE_Secure.ino
+++ examples/SerialToSerialBLE_Secure/SerialToSerialBLE_Secure.ino
@@ -6,19 +6,18 @@
 // #include <etl/circular_buffer.h>
 
 BLESerial SerialBLE;
-// If you are using older version of Arduino IDE, you may need to use
-// empty template argument (<>), due to the old C++ compiler version (<=std=c++17).
+// If you are using an older version of Arduino IDE or C++ compiler, you may need to use
+// an empty template argument (<>), as Class Template Argument Deduction (CTAD) is not
+// supported in C++ versions older than C++17. For more details, see:
 // https://www.cppreference.com/w/cpp/language/ctad.html
 //
-// Uncomment the line below if you are using an older version of Arduino IDE
+// Uncomment the line below if you are using an older version of Arduino IDE/C++ compiler
 // BLESerial<> SerialBLE;
 
 // FOR ETL: Uncomment one of the following lines
 // BLESerial<etl::queue<uint8_t, 255, etl::memory_model::MEMORY_MODEL_SMALL>> SerialBLE;
 // OR
 // BLESerial<etl::circular_buffer<uint8_t, 255>> SerialBLE;
-
-uint32_t passKey;
 
 class AppSecurityCallbacks : public BLESecurityCallbacks {
   public:
